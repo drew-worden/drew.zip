dcbb3c4f8195eaebf05f01737a76afd648d0aacf
drew-worden <drew.p.worden@gmail.com>
2024-11-23 21:07:44 -0500

    Added robots.txt.

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,7 +6,10 @@
 <div class="page col-center">
 	<span class="small-text">404</span>
 	<p class="message">{$page.error?.message}</p>
-    <a href="/" class="button">Back to Home</a>
+	<a
+		href="/"
+		class="button">Back to Home</a
+	>
 </div>
 
 <style>
@@ -17,20 +20,20 @@
 		top: 0;
 		left: 0;
 		justify-content: center;
-        z-index: -1;
-        gap: 8px;
+		z-index: -1;
+		gap: 8px;
 	}
 
-    .message {
-        font-family: satoshi;
+	.message {
+		font-family: satoshi;
 		font-weight: 400;
-        color: var(--font-color);
-        font-size: 64px;
-    }
+		color: var(--font-color);
+		font-size: 64px;
+	}
 
-    @media (max-width: 600px) {
-        .message {
-            font-size: 48px;
-        }
-    }
+	@media (max-width: 600px) {
+		.message {
+			font-size: 48px;
+		}
+	}
 </style>
