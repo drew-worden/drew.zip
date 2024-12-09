3cf32df586a38d70c42f7a1ac3ee99ce3d4e4a52
drew-worden <drew.p.worden@gmail.com>
2024-12-09 12:07:12 -0500

    Added resume page.

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,31 @@
+<div class="page">
+    <iframe class="resume" src="resume.pdf" title="Resume"></iframe>
+</div>
+
+<style>
+    .page {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .resume {
+        width: 600px;
+        margin-top: 96px;
+        height: 100vh;
+    }
+
+    @media (max-width: 810px) {
+        .resume {
+            width: calc(100% - 16px);
+            margin-top: 82px;
+        }
+    }
+
+    @media (max-width: 600px) {
+        .resume {
+            width: calc(100% - 8px);
+            margin-top: 64px;
+        }
+    }
+</style>
